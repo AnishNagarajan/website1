5a22b7d385123ca2ebd360b25f6387c35074b8eb
AnishElon <anishelon@gmail.com>
2024-05-03 22:23:45 +0530

    contact-us-page-common-header-and footer added

---
+++ assets/js/headerfootermodule.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  async function importHeaderFooter() {
+    const responseHeader = await fetch("../header.html");
+    const responseFooter = await fetch("../footer.html");
+    const headerHTML = await responseHeader.text();
+    const footerHTML = await responseFooter.text();
+    document.body.innerHTML = headerHTML + document.body.innerHTML + footerHTML;
+  }
+  importHeaderFooter();
+});
