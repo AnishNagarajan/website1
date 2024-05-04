d378539b3072b00685976fac4f1b00b0c363fbf9
AnishElon <anishelon@gmail.com>
2024-05-04 23:37:09 +0530

    changes added

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var ham = document.querySelector(".hamburger");
-  var navigation = document.querySelector(".navigation");
-  ham.addEventListener("click", function () {
-    navigation.classList.toggle("showHamburger");
-  });
+var ham = document.querySelector(".hamburger");
+var navigation = document.querySelector(".navigation");
+ham.addEventListener("click", function () {
+  navigation.classList.toggle("showHamburger");
 });
